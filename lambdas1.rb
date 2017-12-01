048af32fc5e89d17f68b9539fff8f7a301ba5200
nunonas <nuno_nas@netcabo.pt>
2017-12-01 01:37:07 +0000

    First fucking commit!

---
+++ lambdas1.rb
@@ -0,0 +1,9 @@
+my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+
+# Add your code below!
+
+symbol_filter = lambda{ |x| x.is_a? Symbol}
+
+symbols = my_array.select(&symbol_filter)
+
+puts symbols
